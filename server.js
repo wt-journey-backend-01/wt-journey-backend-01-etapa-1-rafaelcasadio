75017eff04ff0f03bd400a711d1edb63d63c1999
Rafael <rafaelcasadio2@gmail.com>
2025-07-09 22:52:37 -0300

    feat: adiciona novas rotas no servidor para index e contato

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.status(200).sendFile(path.join(__dirname+'/views/index.html'));
 });
+
+app.get('/contato', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname+'/views/contato.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
