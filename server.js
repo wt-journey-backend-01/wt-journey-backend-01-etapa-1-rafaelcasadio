9675265e58d285d78fb41ba807baff5c58d5bc44
Rafael <rafaelcasadio2@gmail.com>
2025-07-10 00:49:52 -0300

    feat: adiciona post contato

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/views/index.html'));
 });
@@ -14,14 +16,24 @@ app.get('/', (req, res) => {
 app.get('/sugestao', (req, res) => {
     const { nome, ingredientes } = req.query;
 
-    res.send(`
+    res.status(200).send(`
     <h1>Obrigado pela sugestão, ${nome}!</h1>
     <p>Você sugeriu os ingredientes:${ingredientes}.</p>
   `);
-})
+});
 
 app.get('/contato', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/views/contato.html'));
+});
+
+app.post('/contato', (req, res) => {
+    const { nome, email, assunto, mensagem } = req.body;
+    res.status(200).send(`
+    <h1>Obrigado pelo contato, ${nome}!</h1>
+    <p>Email: ${email}</p>
+    <p>Assunto: ${assunto}</p>
+    <p>Menssagem enviada: ${mensagem}</p>
+    `);
 });
 
 app.get('/api/lanches', (req, res) => {
